48ecf9a0d85c9e9e454d34df0d0b38769aff3e04
Colin <colin.diesh@gmail.com>
2022-11-27 13:49:22 -0500

    Initial commit

---
+++ build/md_parser.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin, // or fileStream
+})
+
+let readingHeader = false
+let title = ''
+let topLevel = false
+
+;(async () => {
+  for await (const line of rl) {
+    if (line.startsWith('import Figure')) {
+      continue
+    }
+    if (line.startsWith('import config')) {
+      continue
+    }
+    if (!readingHeader && line === '---') {
+      readingHeader = true
+      continue
+    }
+
+    if (readingHeader && line.startsWith('title')) {
+      title = line.replace('title: ', '')
+      continue
+    }
+    if (readingHeader && line.startsWith('toplevel')) {
+      topLevel = true
+    }
+    if (readingHeader && line === '---') {
+      readingHeader = false
+      if (topLevel) {
+        console.log(`\n\n\n## ${title}`)
+      } else {
+        console.log(`\n### ${title}`)
+      }
+      continue
+    }
+    if (readingHeader === false) {
+      if (line.startsWith('#')) {
+        console.log(`#${line}`)
+      } else {
+        console.log(line)
+      }
+    }
+  }
+})()
---
+++ build/title.md
@@ -0,0 +1,6 @@
+---
+title: Learn Genomics
+date: \today
+subtitle: A guide for software engineers
+geometry: "left=2cm,right=2cm,top=2cm,bottom=2cm"
+---
